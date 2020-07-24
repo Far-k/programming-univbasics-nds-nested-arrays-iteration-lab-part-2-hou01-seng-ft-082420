d5f20874d60e1c5c302a249a02428db4fa642c3f
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-24 04:44:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ def find_min_in_nested_arrays(src)
   array_of_daily_temp = []
   row_index = 0
 
-  while row_index < src.count do 
+  while row_index < src.count do
     element_index = 0
     min_integer_element = 0
       while element_index < src[row_index].count do
